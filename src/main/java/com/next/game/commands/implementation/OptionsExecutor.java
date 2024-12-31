b08eddd833adb60f65e412befd8184f4acd61ddf
cristhian-a <lasanhagg@gmail.com>
2024-12-31 19:44:01 -0300

    Alterações no executor de blocos de script

--- src/main/java/com/next/game/commands/implementation/OptionsExecutor.java
+++ src/main/java/com/next/game/commands/implementation/OptionsExecutor.java
@@ -4,6 +4,8 @@ import com.next.game.commands.CommandExecutor;
 import com.next.graphics.TextPrinter;
 import com.next.io.InputReader;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 public class OptionsExecutor implements CommandExecutor {
@@ -16,9 +18,17 @@ public class OptionsExecutor implements CommandExecutor {
 
     @Override
     public void execute(String arguments, Map<String, Object> context) {
-        String[] options = arguments.split("\n");
-        for (String s : options) {
-            TextPrinter.typeText(s);
+        TextPrinter.println("");
+
+        String[] splitByOption = arguments.split("\\\\option");
+        List<String> options = Arrays.stream(splitByOption).filter(x -> !x.isBlank()).toList();
+        for (int i = 0; i < options.size(); i++) {
+            String text = "\t[" + (i + 1) + "] " + options.get(i) + "\n";
+            TextPrinter.typeTextQuickly(text);
         }
+
+        TextPrinter.println("");
+        String input = inputReader.read();
+        context.put("input", input);
     }
 }
